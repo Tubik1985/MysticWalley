7a38246deb3a2bb772aa42f00c5357f8f2967ff8
Tubik1985 <tubtubik@ya.ru>
2025-09-09 11:57:53 +0300

    Фон, страница предсказаний+ карточки героев

--- obj/Debug/net8.0-ios/iossimulator-x64/MysticWalley.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/MysticWalley.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MysticWalley")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d59b982dc0c746e49da54acfc3b18d4c2fbfbd53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+34e7685633396491c02ea509a0712ee167006c45")]
 [assembly: System.Reflection.AssemblyProductAttribute("MysticWalley")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MysticWalley")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
