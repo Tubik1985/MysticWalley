b21935925afc76437eb717193773001a040a204c
Tubik1985 <tubtubik@ya.ru>
2025-09-09 10:29:08 +0300

    Первый коммит: инициализация проекта MysticWalley

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MysticWalley
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MysticWalley
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
