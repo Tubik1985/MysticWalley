e8ac924001147ee411a7ac04d68f158badfdb7e4
Tubik1985 <tubtubik@ya.ru>
2025-10-13 18:24:47 +0300

    fix: synced WhisperPage and HistoryPage after release build

--- obj/Debug/net8.0-android/MysticWalley.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/MysticWalley.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MysticWalley")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5c204d7bb206411aa581c8d655d3e0749bf6e46a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f068e20ea138a90006f23a7d62ac8130097b3158")]
 [assembly: System.Reflection.AssemblyProductAttribute("MysticWalley")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MysticWalley")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
